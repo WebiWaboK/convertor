3d31a57f8434e164e5d66082514fc2c8d025f13a
WebiWaboK <pepin21112003@gmail.com>
2024-05-03 22:56:35 -0600

    aun nada dios yaaaaaaaa

--- database.js
+++ database.js
@@ -1,27 +1,12 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://usuario:contraseña@localhost:3306/converter');
 
-const mysql2 = require('mysql2');
-
-// Crea un pool de conexiones
-const pool = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'converter',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-// Exporta el pool de conexiones para que pueda ser utilizado en otros archivos
-module.exports = pool.promise(); // Utiliza el método `promise()` para trabajar con promesas
-
-// Intenta conectarte a la base de datos utilizando el pool de conexiones
-(async () => {
-    try {
-        const connection = await pool.getConnection();
-        connection.release(); // Libera la conexión para que pueda ser utilizada por otros
-        console.log('Conexión establecida :D a la base de datos');
-    } catch (error) {
-        console.error('Error al conectar a la base de datos:', error.message);
-    }
-})();
+// Intenta autenticar la conexión a la base de datos
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexión establecida con éxito :D');
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
+  });
